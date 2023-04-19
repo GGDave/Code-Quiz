060fcb2c944aec5965a200ec2415ff220fec91e2
David Flores <dcflores@live.com>
2023-04-19 10:32:26 -0700

    JS file added for organization and reference

---
+++ questions.js
@@ -0,0 +1,28 @@
+let questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____?",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "String values must be enclosed within ____ when  being assigned to a variables?",
+      choices: ["Commas", "curly brackets", "quotes", "parenthesis"],
+      answer: "parenthesis"
+    },
+    {
+      title: "Arrays in JavaScipt can be used to store?",
+      choices: ["number of strings", "other strings", "booleans", "all the above"],
+      answer: "all the above"
+    },
+    {
+      title: "A very usefull tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "Terminal/Bash", "for loops", "console log"],
+      answer: "console log"
+    },
+    
+  ]
